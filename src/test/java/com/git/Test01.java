3866dda353e7a9999ca60a291f4b25f989565237
mcrcr <13722812713@163.com>
2022-01-06 20:42:23 +0800

    test01修改

---
+++ src/test/java/com/git/Test01.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
+    }
+}
